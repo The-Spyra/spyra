1c5396b883bbfa4e48b477de9594d775cd543765
shadinmhd <108604275+shadinmhd@users.noreply.github.com>
2023-09-10 20:26:31 +0530

    made landing page and navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <title>Spyra</title>
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    />
+</svelte:head>
+
+<header
+    class="flex fixed h-12 top-0 left-0 w-screen text-white justify-between items-center px-10 py-10"
+>
+    <a href="/" class="font-bold text-4xl -tracking-[1.5px]">SPYRA</a>
+    <nav>
+        <ul class="flex gap-6 text-lg">
+            <li><a class="transition-colors duration-500 bg-clip-text hover:text-transparent ease-in-out font-bold text-white bg-gradient-to-br from-yellow-500 to-red-500" href="/">Home</a></li>
+            <li><a class="transition-colors duration-500 bg-clip-text hover:text-transparent ease-in-out font-bold text-white bg-gradient-to-br from-yellow-500 to-red-500" href="/about">About</a></li>
+            <li><a class="transition-colors duration-500 bg-clip-text hover:text-transparent ease-in-out font-bold text-white bg-gradient-to-br from-yellow-500 to-red-500" href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<div class="font-sans text-white">
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<div>
+    <section
+        class="pt-12 cursor-default flex gap-10 snap-start flex-col items-center justify-center h-screen text-white"
+    >
+        <div class="flex text-7xl flex-col">
+            <p class="font-bold">
+                Discover <span
+                    class="transition-colors bg-clip-text text-transparent bg-gradient-to-tr duration-500 ease-in-out hover:from-blue-500 hover:to-green-500 from-red-500 to-yellow-500"
+                    >the</span
+                >
+            </p>
+            <p class="font-bold">
+                Power <span
+                    class="transition-colors bg-clip-text text-transparent bg-gradient-to-tr duration-500 ease-in-out hover:from-blue-500 hover:to-green-500 from-red-500 to-yellow-500"
+                    >of</span
+                > design
+            </p>
+        </div>
+        <div>
+            <button
+                class="bg-white opacity-100 text-black rounded-lg font-semibold py-2 px-3"
+            >
+                Explore
+            </button>
+        </div>
+    </section>
+    <section
+        class="h-screen snap-start flex-col pt-12 flex items-center justify-center gap-5"
+    >
+        <i class="fa-solid fa-person-digging fa-2xl" style="color: yellow;" />
+        <p class="text-4xl font-bold">under construction</p>
+    </section>
+    <section
+        class="h-screen snap-start flex-col pt-12 flex items-center justify-center gap-5"
+    >
+        <i class="fa-solid fa-person-digging fa-2xl" style="color: yellow;" />
+        <p class="text-4xl font-bold">under construction</p>
+    </section>
+</div>
